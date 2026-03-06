96fa3f768d86bce19c4c98cfceee84ad83c7b136
Joe Pascual <joe.pascual@dotstudioz.com>
2026-03-06 11:37:51 -0500

    Add ticket link for NC show (The Acorn Center, May 15)

--- client/src/hooks/useCalendarEvents.ts
+++ client/src/hooks/useCalendarEvents.ts
@@ -33,7 +33,7 @@ const STATIC_EVENTS: CalendarEvent[] = [
     start: new Date("2026-05-15T19:30:00"),
     end: new Date("2026-05-15T23:00:00"),
     location: "411 Mosby Ave, Littleton, NC 27850, USA",
-    description: "",
+    description: "https://www.etix.com/ticket/p/99678040",
   },
   {
     id: "5",
